1ed35dd27b9b3724c7df04b968ff8dda83e341c6
Charlie Hall <Charlie@Charlies-MacBook-Pro.local>
2015-04-22 13:14:36 +0100

    first rev of project

---
+++ Swift ios8 tutorial/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Swift ios8 tutorial
+//
+//  Created by Charlie Hall on 20/04/2015.
+//  Copyright (c) 2015 Charlie Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
+
+}
+
